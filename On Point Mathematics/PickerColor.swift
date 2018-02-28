4c704058d17f6d4ccdf8792455c21dd0decd57f5
Emmett S <emmett0502@outlook.com>
2018-02-27 21:30:59 -0600

    Changes Default Colors

---
+++ On Point Mathematics/PickerColor.swift
@@ -0,0 +1,9 @@
+//
+//  PickerColor.swift
+//  On Point Mathematics
+//
+//  Created by Emmett S. on 2/27/18.
+//  Copyright © 2018 Emmett S. All rights reserved.
+//
+
+import Foundation
---
+++ On Point Mathematics/TabColors.swift
@@ -0,0 +1,9 @@
+//
+//  TabColors.swift
+//  On Point Mathematics
+//
+//  Created by Emmett S. on 2/27/18.
+//  Copyright © 2018 Emmett S. All rights reserved.
+//
+
+import Foundation
